23faab35fddd2c0c855156bf9b521381bd615fec
SebastianBalZa <seb.balboa@duocuc.cl>
2024-08-31 13:12:34 -0400

    cambio1

--- src/app/registro/registro.page.ts
+++ src/app/registro/registro.page.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-registro',
   templateUrl: './registro.page.html',
   styleUrls: ['./registro.page.scss'],
 })
-export class RegistroPage implements OnInit {
+export class RegistroPage {
+  nombre: string = '';
+  email: string = '';
+  password: string = '';
 
   constructor() { }
 
-  ngOnInit() {
+  onRegister() {
+    console.log('Registrando:', this.nombre, this.email, this.password);
   }
-
 }
